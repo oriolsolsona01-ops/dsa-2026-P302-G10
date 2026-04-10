34b80f3b452e2785ce9495beda8f390c61108aaa
oriolsolsona-01 <oriol.solsona01@estudiant.upf.edu>
2026-04-10 12:25:52 +0200

    canvitasca1-4

---
+++ src/lab2.c
@@ -0,0 +1,6 @@
+#include "sample_lib.h"
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
---
+++ src/request_maporiol.c
@@ -0,0 +1,57 @@
+#include "sample_lib.h"
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+void ask_map (){ // tasca 1 + nota 1 
+    // xs_1, xs_2, md_1, lg_1, xl_1 or 2xl_1
+    char mapa[50];
+    
+    scanf("Introdueix un nom de mapa: xs_1, xs_2, md_1, lg_1, xl_1 or 2xl_1: %s", mapa);
+
+    //creem un string amb la ruta sencera del filename
+    char rutafinal [150];
+    sprintf(rutafinal, "maps/%s/houses.txt", mapa); //escriu tot el contingut a rutafinal
+    
+    FILE *fitxer = fopen(rutafinal, "r");
+    if (fitxer == NULL) {
+        printf("Error: No s'ha pogut obrir el fitxer.\n");
+        return 1;
+    }
+}
+
+// -- ORIGIN
+
+void input_originposition(){ // tasca 2,3 i 4
+
+    int posicio_origen;
+    int num;
+    char street_name[100];
+    scanf("Com voleu introduir la vostra posició d'origen? Adreça (1), Lloc (2) or Coordenada (3) %d", &posicio_origen);
+    while (posicio_origen < 1 && posicio_origen > 3){
+        scanf("Com voleu introduir la vostra posició d'origen? Adreça (1), Lloc (2) or Coordenada (3) %d", &posicio_origen);
+
+    }
+    if (posicio_origen == 2 || posicio_origen == 3){
+
+        printf("Encara no hem implementat aquesta funcionalitat");
+
+    }
+    
+    else{
+
+        scanf("Introdueix el nom del carrer %s", street_name);
+        scanf("Introdueix el número del carrer %d", num);
+        //cal trobar les coordenades un cop tenint aquestes dades amb el nom i num amb fscanf
+        char* coordenades;
+    }
+
+    
+    
+}
+
+
+
+
+
