f0d39124a76279186aee3d5b0ce145215caa6e03
oriolsolsona-01 <oriol.solsona01@estudiant.upf.edu>
2026-04-14 17:30:39 +0200

    canvi14/4

--- src/request_maporiol.c
+++ src/request_maporiol.c
@@ -1,10 +1,11 @@
 #include "sample_lib.h"
+#include "house.h"
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 
-void ask_map (){ // tasca 1 + nota 1 
+FILE* ask_map (){ // tasca 1 + nota 1 (la funció retorna el punter al document o NULL)
     // xs_1, xs_2, md_1, lg_1, xl_1 or 2xl_1
     char mapa[50];
     
@@ -17,25 +18,46 @@ void ask_map (){ // tasca 1 + nota 1
     FILE *fitxer = fopen(rutafinal, "r");
     if (fitxer == NULL) {
         printf("Error: No s'ha pogut obrir el fitxer.\n");
-        return 1;
+        return NULL;
     }
+
+        return fitxer; 
 }
 
+    void houselinked_list (FILE *fitxer){
+        while (!eof) {
+            add_house()
+            fscanf()
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
 // -- ORIGIN
 
-void input_originposition(){ // tasca 2,3 i 4
+void input_originposition(FILE* fitxer){ // tasca 2,3 i 4 (el paràmtre d'entrada serà el punter al doc)
 
     int posicio_origen;
     int num;
     char street_name[100];
     scanf("Com voleu introduir la vostra posició d'origen? Adreça (1), Lloc (2) or Coordenada (3) %d", &posicio_origen);
-    while (posicio_origen < 1 && posicio_origen > 3){
+    while (posicio_origen < 1 || posicio_origen > 3){
         scanf("Com voleu introduir la vostra posició d'origen? Adreça (1), Lloc (2) or Coordenada (3) %d", &posicio_origen);
 
     }
     if (posicio_origen == 2 || posicio_origen == 3){
 
-        printf("Encara no hem implementat aquesta funcionalitat");
+        printf("Encara no hem implementat aquesta funcionalitat. \n");
 
     }
     
