2160e03b2b24272d64f854dce4c700aa1f4d847b
EloiSantiveri <eloi.santiveri01@estudiant.upf.edu>
2026-04-14 17:29:44 +0200

    find_house

--- src/houses.c
+++ src/houses.c
@@ -1,4 +1,5 @@
 #include "houses.h"
+#include "sample_lib.h"
 
 HouseNode* add_house(HouseNode* head, char* street, int number, double lat, double lon) {
     HouseNode* new_house = (HouseNode*)malloc(sizeof(HouseNode));
@@ -11,4 +12,34 @@ HouseNode* add_house(HouseNode* head, char* street, int number, double lat, doub
     new_house->next = head;
 
     return new_house;
+}
+
+HouseNode* find_house(HouseNode* head, char* target_street, int target_number) {
+    HouseNode* current = head;
+    
+    HouseNode* best_match = NULL;
+    int min_distance = 999999;
+
+    while (current != NULL) {
+        // Calculem la distància d'ortografia
+        int dist = LevenstheinDistance(current->street_name, target_street);
+
+        // Si és una coincidència PERFECTA (distància 0) i el número quadra
+        if (dist == 0 && current->house_number == target_number) {
+            return current;
+        }
+
+        // Si la distància és menor que el nostre rècord actual, actualitzem el rècord
+        // (Nota: per ara ignorem el número de casa aquí, busquem el carrer més semblant)
+        if (dist < min_distance) {
+            min_distance = dist;
+            best_match = current;
+        }
+
+        current = current->next;
+    }
+
+    // Si hem acabat el bucle i no hem trobat una coincidència perfecta (0),
+    // retornem la casa del carrer que s'assemblava més
+    return best_match;
 }
